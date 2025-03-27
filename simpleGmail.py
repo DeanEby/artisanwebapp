3609bcfe97d713cca9229d02f673d16c23766060
DeanEby <ebyd21@gmail.com>
2025-03-27 11:44:55 -0400

    refactor: clean up code

--- simpleGmail.py
+++ simpleGmail.py
@@ -4,47 +4,39 @@ import re
 gmail = Gmail('secrets/credentials.json')
 
 def cleanMessageHTML(message):
+    regex = re.compile('<.*?>')
     message = message.replace('</td>', '##SPLIT##')
+    message = message.replace(':', '')
     message = re.sub(regex, '', message)
     message = message.replace('\n', '')
+    message = message.replace('\r', ' ')
     return message
 
 
-messages = gmail.get_unread_inbox()
-regex = re.compile('<.*?>')
-all_messages_list = []
-for message in messages:
-    message_html = cleanMessageHTML(message.html)
-    message_list = message_html.split('##SPLIT##')
-    message_list = [line for line in message_list if line]
-    all_messages_list.append(message_list)
-    #print(message_list)
-    ##print(repr(message_html))
+def getUnreadMessagesAsDict(gmail):
+    messages = gmail.get_unread_inbox()
+    all_messages_list = []
+    for message in messages:
+        message_html = cleanMessageHTML(message.html)
+        message_list = message_html.split('##SPLIT##')
+        message_list = [line for line in message_list if line]
+        all_messages_list.append(message_list)
 
-message_dict_list = []
-for message in all_messages_list:
-    message_dict = {}
-    for index, item in enumerate(message):
-        if index % 2 == 0:
-            if index < len(message) -1:
-                message_dict[item] = message[index + 1]
-    #print(message_dict)
-    message_dict_list.append(message_dict)
+    message_dict_list = []
+    for message in all_messages_list:
+        message_dict = {}
+        for index, item in enumerate(message):
+            if index % 2 == 0:
+                if index < len(message) -1:
+                    message_dict[item] = message[index + 1]
+        message_dict_list.append(message_dict)
 
-    print(message_dict_list)
+        
+    return message_dict_list
+    
+output = getUnreadMessagesAsDict(gmail)
+
+print(output)
 
 
-
-
-
-    # message_html.replace('<body><table>', '')
-    # message_html.replace('<tr>', '')
-    # message_html.replace('<<td valign="top">', '')
-
-    #print(f'HTML: {message.html}')
-
-    # if message.attachments:
-    #     for attm in message.attachments:
-    #         print(f'Attachment: {attm.filename}')
-    #         attm.save(filepath=f'message{message_count}.csv')
 
